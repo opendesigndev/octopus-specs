380aa7b15bd75e7f1025cefff8cebb2b6b69d372
Jan Roztočil <janroztocil@avocode.com>
2022-03-31 15:03:48 +0200

    Added Manifest specification and TS types

---
+++ packages/manifest-ts/prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'singleQuote': true,
+  'quoteProps': 'preserve',
+  'jsxSingleQuote': true,
+  'trailingComma': 'es5',
+  'bracketSpacing': true,
+  'bracketSameLine': false,
+  'arrowParens': 'always',
+  'printWidth': 80,
+  'tabWidth': 2,
+  'semi': false,
+}
